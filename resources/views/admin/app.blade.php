1196e1544440be7a850da493f2e388f2753c1fcc
Cù Kim Ngọc <polyxgo@gmail.com>
2025-11-05 04:21:40 +0700

    chore: setup PolyCMS base (Sanctum, Permission, Lighthouse, Vue admin shell)

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PolyCMS Admin</title>
+    @vite('resources/js/admin/main.ts')
+</head>
+<body class="antialiased">
+    <div id="polycms-admin-app"></div>
+</body>
+</html>
